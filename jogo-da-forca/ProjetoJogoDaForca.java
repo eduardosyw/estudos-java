5b9af885365e82e4b8c0c8843d27f782e988a3bb
eduardosyw <eduardosyw@gmail.com>
2026-09-10 10:56:35 -0300

    Add files via upload
    código do jogo da forca

---
+++ jogo-da-forca/ProjetoJogoDaForca.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+public class ProjetoJogoDaForca {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		ArrayList<String> palavrasSecretas = new ArrayList<>();
+		palavrasSecretas.add("espada");
+		palavrasSecretas.add("cachorro");
+		palavrasSecretas.add("geladeira");
+
+		Random random = new Random();
+		int tamanhoDoArrayList = palavrasSecretas.size();
+		int indiceDaPalavraAleatoriaGerada = random.nextInt(tamanhoDoArrayList);
+		String palavraSecreta = palavrasSecretas.get(indiceDaPalavraAleatoriaGerada); // escolhe uma palavra secreta aleatóriamente
+
+		ArrayList<Character> letrasDescobertas = new ArrayList<>();
+
+		for (int i = 0; i < palavraSecreta.length(); i++) {
+			letrasDescobertas.add('_');
+		}
+		
+		int tentativas = 6;
+		boolean palavraFoiDescoberta = false;
+
+		while (!palavraFoiDescoberta && tentativas > 0) {
+			System.out.println("Palavra: " + letrasDescobertas);
+			System.out.print("Chute uma letra: ");
+			char chute = scanner.next().charAt(0);
+
+			boolean acertou = false;
+			for (int i = 0; i < palavraSecreta.length(); i++) {
+				if (palavraSecreta.charAt(i) == chute) {
+					letrasDescobertas.set(i, chute); // se o chute for certo, coloco na lista de descobertas no indice correto
+					acertou = true;
+				}
+			}
+
+			if (!acertou) {
+				tentativas--;
+				System.out.println("Você tem mais " + tentativas + " tentativas.");
+			}
+
+			palavraFoiDescoberta = !letrasDescobertas.contains('_'); // se a palavra não contém nenhum traçinho, é pq foi completamente descoberta
+		}
+
+		if (palavraFoiDescoberta) {
+			System.out.println("Parabéns, você acertou! A palavra era: " + palavraSecreta);
+		} else {
+			System.out.println("Você perdeu! A palavra era: " + palavraSecreta);
+		}
+	}
+}
